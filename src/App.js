657287a2c993088328ecff49b59e0d2ce13bac8b
Wade Stewart <stew.wade@gmail.com>
2019-01-21 18:44:01 -0500

    initial commit, moved components from art-history

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import Header from '../header/Header'
+import ArtList from '../art-list/ArtList'
+import ArtDetail from '../art-detail/ArtDetail'
+import Flashcard from '../flashcard/Flashcard'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    // Binding `this` for callbacks
+    this.handleShowLikes = this.handleShowLikes.bind(this)
+    this.handleArtDetailClick = this.handleArtDetailClick.bind(this)
+
+    this.state = {
+      artworks: [],
+      image: [],
+      current:  {},
+      likes: [],
+      currentIndex: 0,
+      timer: 2,
+      show: false
+    }
+  }
+
+  handleShowLikes = (artwork) => {
+    const likes = this.state.likes.slice()
+    const artworkIndex = likes.indexOf(artwork)
+
+    if (artworkIndex > -1) {
+      likes.splice(artworkIndex, 1)
+    } else {
+      likes.push(artwork)
+    }
+    this.setState({ likes })
+  }
+
+  handleArtDetailClick = (artwork) => {
+    fetch(`https://art-history-back.herokuapp.com/${artwork.id}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState(prevState => ({ show: !prevState.show }))
+        this.setState({ current: data.object })
+        this.setState({ image: this.state.current.images[0].z })
+      })
+      .catch(err => console.log(err))
+  }
+
+  next = () => {
+    let nextIndex = 
+      this.state.currentIndex +1 !== this.state.artworks.length
+      ? this.state.currentIndex +1
+      : this.state.currentIndex
+
+    this.setState({
+      currentIndex: nextIndex,
+    })
+  }
+
+  componentDidMount = () => {
+    fetch('https://art-history-back.herokuapp.com/')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ artworks: data.objects })
+      })
+      .catch(err => console.log(err))
+  }
+      
   render() {
+    // console.log(this.state.images)
+    let artList =
+      this.state.artworks.length !== 0 ?
+        <ArtList
+          artworks={this.state.artworks}
+          likes={this.state.likes}
+          onShowLikes={this.handleShowLikes}
+          onArtDetailClick={this.handleArtDetailClick}
+        />
+      :
+        null
+
+    let flashcard = this.state.artworks[this.state.currentIndex]
+
+    // let artCarousel = 
+    //   (this.state.current !== true && this.state.show !== true)
+    //   ? (flashcard !== undefined)
+    //     ? <Flashcard
+    //         flashcard={flashcard}
+    //         onTimerEnd={this.next}
+    //       />
+    //     : null 
+    //   : <ArtDetail
+    //       artwork={this.state.current}
+    //       image={this.state.image}
+    //     />
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <div className="art-library">
+          {artList}
+          <ArtDetail
+            artwork={this.state.current}
+            image={this.state.image}
+          />
+          {/* {artCarousel} */}
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <nav>
+                    <h2>Art History</h2>
+                    <h3>Equal parts ignorance and bliss</h3>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header
